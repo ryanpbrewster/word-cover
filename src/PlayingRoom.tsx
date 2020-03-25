24375e88583591dc24a20ddf5ddf0c4ee81a8904
Ryan P. Brewster <RyanPBrewster@gmail.com>
2020-03-25 16:22:49 -0700

    introduce grids

--- src/PlayingRoom.tsx
+++ src/PlayingRoom.tsx
@@ -8,25 +8,30 @@ interface PlayingRoomProps {
   readonly name: string;
 }
 function PlayingRoom({ gameId, name: myName, gameState: game}: PlayingRoomProps) {
-  const board = [];
-  for (let i=0; i < 5; i++) {
-    const row = [];
-    for (let j=0; j < 5; j++) {
-      const word = game.words[5 * i + j];
-      row.push(<WordCard key={j} label={game.mask[word]}><p>{word}</p></WordCard>);
-    }
-    board.push(<GameBoardRow key={i}>{row}</GameBoardRow>);
-  }
-  return <GameBoardWrapper>{board}</GameBoardWrapper>;
+  const cards = game.words.map((word, idx) => {
+    return <WordCard key={idx} label={game.mask[word]}><p>{word}</p></WordCard>;
+  });
+  return <GameBoard>
+    <WordBoardWrapper><WordBoard>{cards}</WordBoard></WordBoardWrapper>
+  </GameBoard>;
 }
 
-const GameBoardWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
+const GameBoard = styled.div`
+  height: 100%;
+  display: grid;
+  grid-template: 1fr 3fr 1fr / 1fr 3fr 1fr;
 `;
-const GameBoardRow = styled.div`
+
+const WordBoardWrapper = styled.div`
+  grid-area: 2/2;
   display: flex;
-  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const WordBoard = styled.div`
+  display: grid;
+  grid-template: 1fr 1fr 1fr 1fr 1fr / 1fr 1fr 1fr 1fr 1fr;
 `;
 
 interface WordCardProps {
@@ -36,20 +41,23 @@ const WordCard = styled.div<WordCardProps>`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex: 1;
 
-  width: 96px;
-  height: 72px;
   border: solid 1px;
   border-radius: 16px;
+
+  min-width: 48px;
+  margin: 4px;
+  padding: 4px;
 
   background-color: ${({label}) => labelColor(label)};
 `;
 function labelColor(label: Label): string {
   switch (label) {
-    case 'one': return 'rgb(245, 142, 135)';
-    case 'two': return 'rgb(150, 177, 255)';
-    case 'neutral': return 'rgb(222, 222, 184)';
-    case 'death': return 'rgb(89, 90, 94)';
+    case 'one': return 'rgb(245, 142, 135, 0.4)';
+    case 'two': return 'rgb(150, 177, 255, 0.4)';
+    case 'neutral': return 'rgb(222, 222, 184, 0.4)';
+    case 'death': return 'rgb(89, 90, 94, 0.4)';
   }
 }
 
